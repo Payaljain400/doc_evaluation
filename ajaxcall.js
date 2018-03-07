2e63d2628615d85c26f8d0a2310c59eba8117e04
payaljain400 <jain.payal400@gmail.com>
2018-03-07 10:23:31 +0530

    some changes in index.js

--- ajaxcall.js
+++ ajaxcall.js
@@ -8,7 +8,7 @@ function getData(){
       if(this.readyState == 4 && this.status == 200){
       	var myArr= JSON.parse(this.responseText);
       	var dataObj= JSON.stringify(myArr);
-      	document.getElementById('button').innerHTML=dataObj;
+      	document.getElementById('show').innerHTML=dataObj;
 
        
     }  
